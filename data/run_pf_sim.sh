fa87f7a6eaf35fd290778bf2d7900f83fa53df5c
eric <emand@uvic.ca>
2015-08-28 17:05:39 -0700

    adding data and configuration files for tests

---
+++ data/run_pf_sim.sh
@@ -0,0 +1,11 @@
+#PBS -l nodes=12:ppn=8
+#PBS -l walltime=336:00:00
+#PBS -j oe
+
+# change the current working directory to the directory where
+# the executable file 'hello' can be found
+cd $PBS_O_WORKDIR
+echo $PBS_O_WORKDIR
+
+# run the executable file 'hello' using the qmpirun script
+mpirun ./../src/bin/psrjmh_ais_auto > ./psrjmh_ais_auto.log
